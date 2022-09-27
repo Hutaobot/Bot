6d4709a5c77141b9fbdd23f2327cbedccf926507
Hutaobot <113575903+Hutaobot@users.noreply.github.com>
2022-09-27 21:54:14 +0700

    Update asupan-loli.js

--- plugins/asupan-loli.js
+++ plugins/asupan-loli.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn, text }) => {
   handler.tags = ['premium']
   handler.help = ['asupanloli']
   handler.premium = true
+  handler.limit = true
   export default handler
   
  global.asupan = [
